66cb43db92ce70cc945fc4722a84bbcbf0695d57
samirkthakur <129790010+samirkthakur@users.noreply.github.com>
2024-12-21 18:27:12 +0000

    first commit

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+const Document = require("./Document")
+
+mongoose.connect("mongodb://127.0.0.1:27017/myapp")    //replace the address with the connection string of the project in MongoDB Atlas and the <db-password> in the connection string with the correct user password
+
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+})
+
+const defaultValue = ""
+
+io.on("connection", socket => {
+  socket.on("get-document", async documentId => {
+    const document = await findOrCreateDocument(documentId)
+    socket.join(documentId)
+    socket.emit("load-document", document.data)
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta)
+    })
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(documentId, { data })
+    })
+  })
+})
+
+async function findOrCreateDocument(id) {
+  if (id == null) return
+
+  const document = await Document.findById(id)
+  if (document) return document
+  return await Document.create({ _id: id, data: defaultValue })
+}
